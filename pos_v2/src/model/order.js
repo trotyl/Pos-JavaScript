68316909944afc1fbea3aa8c04c2d00e8802d531
Trotyl Yu <yzj1995@vip.qq.com>
2014-09-17 16:22:51 +0800

    修改字段名称

--- pos_v2/src/model/order.js
+++ pos_v2/src/model/order.js
@@ -1,5 +1,5 @@
 function Order (items, promotions, list) {
-	this.itemInfo = {};
+	this.items = {};
 	this.total = 0;
 	this.original = 0;
 	this.initiate(items, promotions, list);
@@ -8,17 +8,17 @@ function Order (items, promotions, list) {
 Order.prototype.initiate = function (items, promotions, list) {
 	_(list).each(function (raw_barcode) {
 		barcode = raw_barcode.substring(0,10);
-		var item = this.itemInfo[barcode] || _(items).findWhere({barcode: barcode});
+		var item = this.items[barcode] || _(items).findWhere({barcode: barcode});
 		this.original += item.addCount(raw_barcode);
-		this.itemInfo[barcode] = item;
+		this.items[barcode] = item;
 	}, this);
 
 	var two_with_one_list = _(promotions).findWhere({type: 'BUY_TWO_GET_ONE_FREE'}).barcodes;
 	_(two_with_one_list).each(function (barcode) {
-		this.itemInfo[barcode] && this.itemInfo[barcode].getPromotion();
+		this.items[barcode] && this.items[barcode].getPromotion();
 	}, this);
 
-	this.total = _(this.itemInfo).reduce(function (sum, item) { return sum + item.fare; }, 0, this);
+	this.total = _(this.items).reduce(function (sum, item) { return sum + item.fare; }, 0, this);
 };
 
 Order.prototype.output = function () {
@@ -49,7 +49,7 @@ Order.prototype.getLists = function() {
 
 	var boughtList = '----------------------\n';
 	var freeList = '----------------------\n' + '挥泪赠送商品：\n';
-	_(this.itemInfo).each(function (item) {
+	_(this.items).each(function (item) {
 		boughtList += getBoughtItem(item);
 		item.promotion && (freeList += getFreeItem(item));
 	});
