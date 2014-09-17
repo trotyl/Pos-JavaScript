dbea1912ea909a3cac10f9079d3f1a4370118939
Trotyl Yu <yzj1995@vip.qq.com>
2014-09-17 19:54:28 +0800

    修改名称字段

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -1,6 +1,6 @@
 //TODO: Please write code in this file.
 
 function printInventory (inputs) {
-	var this_order = new Order(loadAllItems(), loadPromotions(), inputs);
-	console.log(this_order.output());
+	var order = new Order(loadAllItems(), loadPromotions(), inputs);
+	console.log(order.output());
 }
