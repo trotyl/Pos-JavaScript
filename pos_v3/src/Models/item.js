c2e75ff86afc70c6deb8befe88f1e2c5394d13da
Trotyl Yu <yzj1995@vip.qq.com>
2015-03-05 15:10:13 +0800

    Create item.js of v3

---
+++ pos_v3/src/Models/item.js
@@ -0,0 +1,8 @@
+function Item (barcode, type, brand, name, unit, quantity) {
+    this.barcode = barcode;
+    this.type = type;
+    this.brand = brand;
+    this.name = name;
+    this.unit = unit;
+    this.quantity = quantity;
+}
