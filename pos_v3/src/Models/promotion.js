a212dc84eefc331b843b6a998ffd0b21bc829845
Trotyl Yu <yzj1995@vip.qq.com>
2015-03-05 15:52:59 +0800

    Updated promotion.js

--- pos_v3/src/Models/promotion.js
+++ pos_v3/src/Models/promotion.js
@@ -9,10 +9,29 @@ Promotion.types = {
     full: 2
 };
 
+Promotion.getPrice = function (promotion, item) {
+    var price = null;
+    if (promotion.isInPromotion(item)) {
+        price = item.price * promotion.discount;
+    }
+    else {
+        price = item.price;
+    }
+    return price;
+};
+
+Promotion.ValidateDiscount = function (discount) {
+    if(discount < 0 && discount > 1){
+        throw new RangeError("The DISCOUNT of promotion is out of range(0, 1)! It's " + discount);
+    }
+};
+
 
 // 单品优惠
-function SinglePromotion(item_barcode) {
+function SinglePromotion(discount, item_barcode) {
+    Promotion.ValidateDiscount(discount);
     this.type = Promotion.types.single;
+    this.discount = discount;
     this.barcode = item_barcode;
 }
 
@@ -22,8 +41,10 @@ SinglePromotion.prototype.isInPromotion = function (item) {
 
 
 // 品牌优惠
-function BrandPromotion(brand_name) {
+function BrandPromotion(discount, brand_name) {
+    Promotion.ValidateDiscount(discount);
     this.type = Promotion.types.brand;
+    this.discount = discount;
     this.brand = brand_name;
 }
 
@@ -33,13 +54,15 @@ BrandPromotion.prototype.isInPromotion = function (item) {
 
 
 // 全场优惠
-function FullPromotion(exception_list) {
+function FullPromotion(discount, exception_list) {
+    Promotion.ValidateDiscount(discount);
     this.type = Promotion.types.full;
+    this.discount = discount;
     this.exceptions = exception_list;
 }
 
 FullPromotion.prototype.isInPromotion = function (item) {
-    return _(this.exceptions).some(function (exception) {
+    return !_(this.exceptions).some(function (exception) {
         return item.barcode == exception
     });
 };
