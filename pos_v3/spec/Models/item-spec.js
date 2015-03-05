7bfe0fc687ef72d10a037220b893f3e9b794665f
Trotyl Yu <yzj1995@vip.qq.com>
2015-03-05 15:18:23 +0800

    Add spec file to item.js and passed

---
+++ pos_v3/spec/Models/item-spec.js
@@ -0,0 +1,20 @@
+describe('Item ', function () {
+    var allItems, inputs, dateDigitToString;
+
+    beforeEach(function () {
+
+    });
+
+    it('should be constructed correctly', function () {
+
+        var item = new Item('ITEM000000','饮料', '可口可乐', '可口可乐350ml', '瓶', 3.00);
+
+        expect(item.barcode).toEqual('ITEM000000');
+        expect(item.type).toEqual('饮料');
+        expect(item.brand).toEqual('可口可乐');
+        expect(item.name).toEqual('可口可乐350ml');
+        expect(item.unit).toEqual('瓶');
+        expect(item.price).toEqual(3.00);
+
+    });
+});
--- pos_v3/src/Models/item.js
+++ pos_v3/src/Models/item.js
@@ -1,8 +1,8 @@
-function Item (barcode, type, brand, name, unit, quantity) {
+function Item (barcode, type, brand, name, unit, price) {
     this.barcode = barcode;
     this.type = type;
     this.brand = brand;
     this.name = name;
     this.unit = unit;
-    this.quantity = quantity;
+    this.price = price;
 }
