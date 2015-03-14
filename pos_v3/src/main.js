e6bab3ee55f7bfae8825be5e85d8fd06f3d2544e
Trotyl Yu <yzj1995@vip.qq.com>
2015-03-14 14:00:15 +0800

    Add comment to main.js

--- pos_v3/src/main.js
+++ pos_v3/src/main.js
@@ -1,6 +1,13 @@
+// The portal of the application.
+// 'input' is a object with the key of barcode and value of amount.
+// 'number' is the number of strategy to select, from 1 to 4, defaults to 1.
+// 'output' is the object with a 'log' method, defaults to 'console'.
 function printInventory(input, number, output) {
+    // Check whether to use default parameter.
     number = number || 1;
     output = output || console;
+
+    // Get the right StrategyFactory of the selected number.
     var strategyFactory;
     switch (number) {
         case 1:
@@ -18,6 +25,10 @@ function printInventory(input, number, output) {
         default :
             throw new Error('Invalid number: ' + number);
     }
+
+    // Get the Strategy from the StrategyFactory.
     var strategy = strategyFactory.GetStrategy();
+
+    // Call the GenerateResult method to run the Strategy.
     strategy.GenerateResult(input, new Formatter(), output);
 }
