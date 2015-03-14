c5ab1943844626ea74b66e16835b1b68fe2e1afe
Trotyl Yu <yzj1995@vip.qq.com>
2015-03-14 14:18:26 +0800

    Move Discount and Promotion to Benefit

--- pos_v3/src/Helpers/benefit.js
+++ pos_v3/src/Helpers/benefit.js
@@ -1,8 +1,38 @@
 function Benefit(){
-
 }
 
 Benefit.types = {
     discount: 0x01,
     promotion: 0x02
+};
+
+
+function Discount(rate, scope, keep) {
+    Discount.Validate(rate);
+    this.type = Benefit.types.discount;
+    this.rate = rate;
+    this.scope = scope;
+    this.keep = keep;
+}
+
+Discount.Validate = function (rate) {
+    if(rate < 0 || rate > 1){
+        throw new RangeError("The rate of rate is out of range(0, 1)! It's " + rate);
+    }
+};
+
+
+function Promotion(from, to, scope, keep) {
+    Promotion.Validate(from, to);
+    this.type = Benefit.types.promotion;
+    this.from = from;
+    this.to = to;
+    this.scope = scope;
+    this.keep = keep;
+}
+
+Promotion.Validate = function (from, reduction) {
+    if(from < 0 || reduction < 0 || from < reduction){
+        throw new RangeError("The promotion condition is illegal");
+    }
 };
